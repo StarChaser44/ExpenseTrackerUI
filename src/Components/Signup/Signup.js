a53fce2da28c2e2e6e1acfd36ba516b71fab3950
Tyree adams <tyree@tyrees-mbp.lan>
2020-05-19 16:41:32 -0400

    Added sign up page without firebase functionality

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './SignupStyle.css'
+const githubLogo = require("../../Images/github_logo.png")
+const gmailLogo = require('../../Images/gmail_logo.png')
 
 /**
  * Signs up customer with our service via manual sign up, gmail, or github
 */
 const Signup = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState(false);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(password !== confirmPassword){
+            setErrorMessage(true);
+        } else {
+            setErrorMessage(false)
+            alert("Thank you for signing up");
+        }
+    }
     return (
-        <div>
-            Sign up page
+        <div className="container">
+            <h1 className="header"> CREATE AN ACCOUNT </h1>
+            <p className="smallText">Or sign in with these accounts</p>
+            <div>
+                <div id="github">
+                    <img src={githubLogo} alt="github logo" />
+                </div>
+                <div id="gmail">
+                    <img src={gmailLogo} alt="gmail logo" />
+                </div>
+            </div>
+            <form className="formContainer" onSubmit={handleSubmit}>
+                <p className="formTextElems">Username</p>
+                <input className="formInputElems" type="text" required="required" onChange = {(e) => setUsername(e.target.value)}/>
+
+                <p className="formTextElems" >Email</p>
+                <input className="formInputElems" type="email" required="required" onChange = {(e) => setEmail(e.target.value)}/>
+            
+                <p className="formTextElems">Password</p>
+                <input className="formInputElems" type="password" required="required" onChange = {(e) => setPassword(e.target.value)}/>
+
+                <p className="formTextElems">Confirm Password</p>
+                <input className="formInputElems" type="password" required="required" onChange = {(e) => setConfirmPassword(e.target.value)}/>
+
+                <button id="submitButton" type="submit"> <p className="submitButtonText">SIGN UP!</p> </button>
+            </form>
+            {
+                errorMessage ? <p id="errorMessage">Passwords do not match!</p> :
+                null
+            }
         </div>
     )
 }
