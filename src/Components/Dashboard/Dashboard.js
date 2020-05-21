940b2277d91851e6ae195c1aa4b624884f93fb7e
Tyree adams <tyree@tyrees-mbp.lan>
2020-05-21 15:30:47 -0400

    Added redirection from home and some dashboard items

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -3,7 +3,30 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div>
-            This is the dashboard
+            <h1>Welcome GODWIN!</h1>
+            <div>
+                <p>Total Income</p>
+                <p>$2,350.57</p>
+                <p>click to set monthly income</p>
+            </div>
+            <div>
+                ADD AND REMOVE INCOME SECTION
+            </div>
+            <div>
+                EXPENSES SECTION
+            </div>
+            <div>
+                <p>Categories</p>
+                <div>
+                    <ul>
+                        <li>Groceries</li>
+                        <li>Entertainment</li>
+                        <li>Transportation</li>
+                        <li>Clothes and shoes</li>
+                        <li>other</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
