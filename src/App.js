e92853e557c338d16325e222c23a455ab1b56cd7
Godwin Olletey <godwinolle@gmail.com>
2020-05-20 20:07:45 -0400

    Added login page without firebase functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import Login from './components/Login/Login';
+import Setup from './components/SetUp/Setup';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+        <div>
+            <Router>
+                <Route exact path = "/login" component={Login} />
+                <Route exact path = "/setup" component={Setup} />
+            </Router>
+        </div>
+    )
 }
 
 export default App;
