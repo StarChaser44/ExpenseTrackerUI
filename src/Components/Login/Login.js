be45d4900603ca0438257684df165dd530534c16
Godwin Olletey <godwinolle@gmail.com>
2020-05-23 13:18:03 -0400

    added login authentication

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,15 +1,32 @@
-import React, {useState}  from 'react';
+import React, { useState }  from 'react';
 import './Login.css';
+
+import { login } from '../../firebase';
+
 const githubLogo = require('../../Images/github_logo.png')
 const gmailLogo = require('../../Images/gmail_logo.png');
+
+
 
 /**
  * Login system for the customer to access their account again
  */
 const Login = () => {
-    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState(''); 
-    const [errorMessage, setErrorMessage] = useState(false);
+    //const [errorMessage, setErrorMessage] = useState(false);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try{
+            await login(email, password);
+            console.log("You logged in successfully!");
+        } catch(err){
+            alert("No boy you thought!");
+        }
+        console.log(email);
+        console.log(password);
+    }
 
     return(
         <div>
@@ -20,14 +37,14 @@ const Login = () => {
                     <img src={gmailLogo} alt="gmail logo" />
                 </div>
                 <div className="loginStuff">
-                    <form className="loginForm">
+                    <form className="loginForm" onSubmit={handleSubmit}>
                         <div className="credentials">
-                            <p className="formTextElems">Username</p>
-                            <input className="formInputElems"/>
+                            <p className="formTextElems">Email</p>
+                            <input className="formInputElems" value={email} onChange={e => setEmail(e.target.value)}/>
                         </div>
                         <div className="credentials">
                             <p className="formTextElems">Password</p>
-                            <input type="password" className="formInputElems"/>
+                            <input type="password" className="formInputElems" value={password} onChange={e => setPassword(e.target.value)}/>
                         </div>
                         <button id="loginButton">login</button>
                     </form>
@@ -35,6 +52,7 @@ const Login = () => {
             </div>
         </div>
     )
+
 }
 
 export default Login;
